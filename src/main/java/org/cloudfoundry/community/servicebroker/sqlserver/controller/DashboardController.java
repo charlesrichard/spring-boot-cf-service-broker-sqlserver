a34f39009e139e3568a5e0b6c0ae3b1205649169
csvoboda-pivotal <csvoboda@pivotal.io>
2015-02-13 17:22:05 -0500

    Removed yaml files.

--- src/main/java/org/cloudfoundry/community/servicebroker/sqlserver/controller/DashboardController.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/sqlserver/controller/DashboardController.java
@@ -1,13 +1,8 @@
 package org.cloudfoundry.community.servicebroker.sqlserver.controller;
 
 import org.cloudfoundry.community.servicebroker.controller.BaseController;
-import org.cloudfoundry.community.servicebroker.controller.ServiceInstanceBindingController;
-import org.cloudfoundry.community.servicebroker.service.ServiceInstanceBindingService;
-import org.cloudfoundry.community.servicebroker.service.ServiceInstanceService;
 import org.cloudfoundry.community.servicebroker.sqlserver.constants.IdentifierConstants;
 import org.cloudfoundry.community.servicebroker.sqlserver.service.DashboardService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
