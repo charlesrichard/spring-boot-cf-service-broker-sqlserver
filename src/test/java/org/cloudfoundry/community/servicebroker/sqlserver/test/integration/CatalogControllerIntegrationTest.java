bb7cfc9cf8ca3eb9c9c8109ae3d11a80e1a28d78
csvoboda-pivotal <csvoboda@pivotal.io>
2015-02-15 17:12:35 -0500

    Catalog tests pass

--- src/test/java/org/cloudfoundry/community/servicebroker/sqlserver/test/integration/CatalogControllerIntegrationTest.java
+++ src/test/java/org/cloudfoundry/community/servicebroker/sqlserver/test/integration/CatalogControllerIntegrationTest.java
@@ -1,57 +1,14 @@
 package org.cloudfoundry.community.servicebroker.sqlserver.test.integration;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.cloudfoundry.community.servicebroker.controller.CatalogController;
-import org.junit.Before;
-import org.junit.Test;
+import org.cloudfoundry.community.servicebroker.service.impl.BeanCatalogServiceTest;
+import org.cloudfoundry.community.servicebroker.sqlserver.config.CatalogConfig;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
-@WebAppConfiguration
-public class CatalogControllerIntegrationTest {
+@ContextConfiguration(classes = {CatalogConfig.class})
+public class CatalogControllerIntegrationTest extends BeanCatalogServiceTest {
 	
-	@Autowired
-    private WebApplicationContext ctx;
-	MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		/*
-		MockitoAnnotations.initMocks(this);
-
-	    this.mockMvc = MockMvcBuilders.standaloneSetup(controller)
-	            .setMessageConverters(new MappingJackson2HttpMessageConverter()).build();
-	            */
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
-	}
-	
-	@Test
-	public void catalogIsRetrievedCorrectly() throws Exception {
-	
-		MvcResult result = this.mockMvc.perform(get(CatalogController.BASE_PATH)
-		        .accept(MediaType.APPLICATION_JSON))
-		        .andExpect(status().isOk())
-	            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-	            .andReturn();
-	    
-	    // TO DO - check rest of the json including plans
-		String content = result.getResponse().getContentAsString();
-		
-		assertNotNull(content);
-	}
 }
